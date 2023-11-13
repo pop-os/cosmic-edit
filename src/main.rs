5c015f1baafa164408a5465e78960ff538cd8e27
Jeremy Soller <jackpot51@gmail.com>
2023-11-13 15:41:15 -0700

    Enable libcosmic wgpu backend for performance

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ lazy_static::lazy_static! {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn")).init();
 
     localize::localize();
 
