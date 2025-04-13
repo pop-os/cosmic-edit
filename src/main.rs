4a8a00a545f456494f06f24b98fef4aab8faf238
Hugo <hugo@pop-os.home>
2025-04-13 13:23:31 -0400

    Argument support with clap_lex

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,10 @@ mod tab;
 use self::text_box::text_box;
 mod text_box;
 
+use clap_lex::RawArgs;
+
+use std::error::Error;
+
 static ICON_CACHE: OnceLock<Mutex<IconCache>> = OnceLock::new();
 static LINE_NUMBER_CACHE: OnceLock<Mutex<LineNumberCache>> = OnceLock::new();
 static SWASH_CACHE: OnceLock<Mutex<SwashCache>> = OnceLock::new();
@@ -79,7 +83,29 @@ pub fn icon_cache_get(name: &'static str, size: u16) -> icon::Icon {
     icon_cache.get(name, size)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let raw_args = RawArgs::from_args();
+    let mut cursor = raw_args.cursor();
+
+    // Parse the arguments
+    while let Some(arg) = raw_args.next_os(&mut cursor) {
+        match arg.to_str() {
+            Some("--help") | Some("-h") => {
+                print_help(env!("CARGO_PKG_VERSION"), env!("VERGEN_GIT_SHA"));
+		return Ok(());
+            }
+            Some("--version") | Some("-V") => {
+		println!(
+		    "cosmic-edit {} (git commit {})",
+                    env!("CARGO_PKG_VERSION"),
+                    env!("VERGEN_GIT_SHA")
+                );
+                return Ok(());
+            }
+            _ => {}
+        }
+    }
+
     #[cfg(all(unix, not(target_os = "redox")))]
     match fork::daemon(true, true) {
         Ok(fork::Fork::Child) => (),
@@ -180,6 +206,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     cosmic::app::run::<App>(settings, flags)?;
 
     Ok(())
+}
+
+fn print_help(version: &str, git_rev: &str) {
+    println!(
+        r#"cosmic-edit {version} (git commit {git_rev})
+System76 <info@system76.com>
+	    
+Designed for the COSMIC™ desktop environment, cosmic-edit is a libcosmic-based text editor.
+	    
+Project home page: https://github.com/pop-os/cosmic-edit
+	    
+Options:
+  -h, --help     Show this message
+  -v, --version  Show the version of cosmic-edit"#
+    );
 }
 
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
